709427e6f0185c2c8b67960d5cd3f0436aafeda2
manimegalai <svmanimozhi@gmail.com>
2025-05-16 15:58:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var a=10;
+var b=12;
+   console.log(a);
+   console.log(b);
+   console.log(a%b);
+   console.log(a+b);
+   console.log(a-b);
+   console.log(a*b);
+   console.log(a/b);
+function add(){
+    var a=10;
+    var b=13;
+    console.log(a+b);
+}
+add()
